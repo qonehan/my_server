49b5450665833eee0c91b6e4e68cadb1a5437c48
qonehan <69864688+qonehan@users.noreply.github.com>
2025-08-15 11:09:34 +0000

    hello world 문구 변경

--- server.js
+++ server.js
@@ -2,10 +2,10 @@
 const express = require('express');
 
 const app = express();              // 서버 앱 만들기
-const PORT = process.env.PORT || 80; // 포트 번호 정하기(환경변수 없으면 3000)
+const PORT = process.env.PORT || 80; // 포트 번호 정하기(환경변수 없으면 80)
 
 app.get("/", (req, res) => {        // "/" 주소로 GET 요청이 오면
-  res.send("Hello World");          // 글자 보내주기(응답)
+  res.send("호유야 안녕 보고싶어 ㅎ");          // 글자 보내주기(응답)
 });
 
 app.listen(PORT, () => {            // 서버를 켠다(대기 시작)
