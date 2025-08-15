3cb3ff30c2848f29f1c0334ba4fd4e9ee5c27fcf
qonehan <69864688+qonehan@users.noreply.github.com>
2025-08-15 10:57:16 +0000

    chore: port 번호 3000에서 80으로 변경

--- server.js
+++ server.js
@@ -2,7 +2,7 @@
 const express = require('express');
 
 const app = express();              // 서버 앱 만들기
-const PORT = process.env.PORT || 3000; // 포트 번호 정하기(환경변수 없으면 3000)
+const PORT = process.env.PORT || 80; // 포트 번호 정하기(환경변수 없으면 3000)
 
 app.get("/", (req, res) => {        // "/" 주소로 GET 요청이 오면
   res.send("Hello World");          // 글자 보내주기(응답)
