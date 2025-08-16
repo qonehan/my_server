f282cc80cac07340fcb2fdb5857ddf8db832d6d7
qonehan <69864688+qonehan@users.noreply.github.com>
2025-08-16 04:13:54 +0000

    feat: html 라우팅

--- server.js
+++ server.js
@@ -1,12 +1,15 @@
 // server.js
 const express = require('express');
+const path = require('path');
 
 const app = express();              // 서버 앱 만들기
 const PORT = process.env.PORT || 80; // 포트 번호 정하기(환경변수 없으면 80)
 
-app.get("/", (req, res) => {        // "/" 주소로 GET 요청이 오면
-  res.send("호유야 안녕 보고싶어 ㅎ");          // 글자 보내주기(응답)
-});
+
+app.get('/', (req, res) => {      // "/" 주소로 GET 요청이 오면
+  res.sendFile(path.join(__dirname, 'public', 'index.html'));
+});       
+
 
 app.listen(PORT, () => {            // 서버를 켠다(대기 시작)
   console.log(`Server on http://localhost:${PORT}`);
